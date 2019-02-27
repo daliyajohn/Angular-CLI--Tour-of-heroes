4b3dfcb9a5f9e4153a7df2d6e2ae7265775cb768
daliyajohn <daliyajohn75@gmail.com>
2019-02-27 18:33:30 +0530

    Route fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutModule } from './layout/layout.module';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { HerosComponent } from './layout/heros/heros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heros', component: HerosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
