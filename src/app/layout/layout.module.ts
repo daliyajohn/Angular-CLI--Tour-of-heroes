d1192d3fa71d9586af616ceb0d5086468db47af6
daliyajohn <daliyajohn75@gmail.com>
2019-02-28 12:32:56 +0530

    Main layout fix

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HerosComponent } from './heros/heros.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent, DashboardComponent, HerosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
